5c722affca7b4e8c0b1945c8ff6a01f4f629d722
ConnorBrookfield <37177602+G00361129@users.noreply.github.com>
2018-04-26 01:28:47 +0100

    Updated navigation bars
    + Added new things
    - Removed old things

---
+++ scripts/navigation.js
@@ -0,0 +1,23 @@
+// Navigation bar Buttons - Event Listeners
+document.getElementById("store").addEventListener("click", function() {pageNavigation("index");});
+document.getElementById("cart").addEventListener("click", function() {pageNavigation("cart");});
+document.getElementById("library").addEventListener("click", function() {pageNavigation("library");});
+document.getElementById("about").addEventListener("click", function() {pageNavigation("about");});
+
+function pageNavigation(page){
+	switch(page)
+	{
+		case "index":
+			window.location.href = "index.html";
+			break;
+		case "cart":
+			window.location.href = "cart.html";
+			break;
+		case "library":
+			window.location.href = "library.html";
+			break;			
+		case "about":
+			window.location.href = "about.html";
+			break;						
+	}
+}
